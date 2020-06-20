e0366756eacf578bc500dda4c08d14e97342f5e2
souryvath <souryvath.nirasay@gmail.com>
2020-06-20 14:47:15 +0200

    api

--- src/app/coronavirus/services/coronavirus-france.service.ts
+++ src/app/coronavirus/services/coronavirus-france.service.ts
@@ -7,16 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class CoronavirusFranceService {
 
-  private readonly urlCSV = 'https://cascoronavirus-1585048636823.appspot.com/france-datas';
-  private readonly urlCSVDay = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-day';
-  private readonly urlTest = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-test';
-  private readonly urlEmergency = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-urgences';
-  private readonly urlTransfert = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-transfert';
-  private readonly urlNews = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-news';
-  private readonly urlOneNews = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-one-news';
-  private readonly urlDeconfinement = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-deconfinement';
-  private readonly urlMortality = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-mortality';
-  private readonly urlTestCentres = 'https://cascoronavirus-1585048636823.appspot.com/france-datas-centre-depistage';
+  private readonly urlCSV = 'https://api.cascoronavirus.fr/france-datas';
+  private readonly urlCSVDay = 'https://api.cascoronavirus.fr/france-datas-day';
+  private readonly urlTest = 'https://api.cascoronavirus.fr/france-datas-test';
+  private readonly urlEmergency = 'https://api.cascoronavirus.fr/france-datas-urgences';
+  private readonly urlTransfert = 'https://api.cascoronavirus.fr/france-datas-transfert';
+  private readonly urlNews = 'https://api.cascoronavirus.fr/france-datas-news';
+  private readonly urlOneNews = 'https://api.cascoronavirus.fr/france-datas-one-news';
+  private readonly urlDeconfinement = 'https://api.cascoronavirus.fr/france-datas-deconfinement';
+  private readonly urlMortality = 'https://api.cascoronavirus.fr/france-datas-mortality';
+  private readonly urlTestCentres = 'https://api.cascoronavirus.fr/france-datas-centre-depistage';
 
   constructor(private readonly httpClient: HttpClient) { }
 
